dc72728e7b5da3652dd008d215262110fa7408fc
Luka Labadze <lukalukalabadze@gmail.com>
2024-07-02 15:48:48 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+function sidebar(){
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'flex'
+}
+
+function hideSidebar(){
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'none'
+}
